96a2029424b4b62dfd547348b6fc636760076bae
Thien Vu Nguyen <nguyencongthienvu@gmail.com>
2019-01-10 09:21:32 +0700

    remove all in vendor.

--- app/Config/fb_config.php
+++ app/Config/fb_config.php
@@ -1,3 +1,4 @@
 <?php
 require_once(APP . 'Vendor' . DS . 'Facebook' . DS . 'src' . DS . 'Facebook' . DS . 'autoload.php');
+require_once(APP . 'Vendor' . DS . 'php-jwt' . DS . 'src' . DS . 'JWT.php');
 ?>
